8f8b4459310196ae73ced384978f08880296ebdb
Sha <shagul1614@gmail.com>
2019-12-15 17:29:12 +0530

    Account management service
    User account managrment service, which controls all user intractive activities

---
+++ SpringBoot+JPA+JSP/BookYourTicket-Account/src/main/java/com/java/ticketbooking/TicketBookerApplication.java
@@ -0,0 +1,13 @@
+package com.java.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookerApplication.class, args);
+	}
+
+}
---
+++ SpringBoot+JPA+JSP/BookYourTicket-Account/src/test/java/com/java/ticketbooking/TicketBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
